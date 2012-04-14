9adbdbb3e6d61210faa1eb1155f06e3c071e8495
Ash Christopher <ash.christopher@gmail.com>
2012-04-13 21:52:43 -0700

    Added files

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os.path
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-bootstrap-static-files',
+    version='2.0.2',  # mapped to Twitter's bootstrap version
+    description='Twitter bootstrap files.',
+    author='Ash Christopher',
+    author_email='ash.christopher@gmail.com',
+    url='https://github.com/ashchristopher/django-bootstrap-static-files',
+    packages=find_packages(),
+    license='Apache License 2.0',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
+
